814db5a8246802c9805c93ff01fd28b6bce8f882
MajidKh230 <khanmajid230@gmail.com>
2024-10-29 23:59:50 +0500

    worked on dashboard menu

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -11,9 +13,11 @@ const Dashboardlayout = ({ children }) => {
 <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
 <span className="hidden md:block font-bold">APS School</span>
 </Link>
+<Menu/>
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar/>
       {children}
       </div>
     </div>
---
+++ src/components/Menu.jsx
@@ -0,0 +1,144 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+
+const Menu = () => {
+    const menuItems = [
+        {
+          title: "MENU",
+          items: [
+            {
+              icon: "/home.png",
+              label: "Home",
+              href: "/",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/teacher.png",
+              label: "Teachers",
+              href: "/list/teachers",
+              visible: ["admin", "teacher"],
+            },
+            {
+              icon: "/student.png",
+              label: "Students",
+              href: "/list/students",
+              visible: ["admin", "teacher"],
+            },
+            {
+              icon: "/parent.png",
+              label: "Parents",
+              href: "/list/parents",
+              visible: ["admin", "teacher"],
+            },
+            {
+              icon: "/subject.png",
+              label: "Subjects",
+              href: "/list/subjects",
+              visible: ["admin"],
+            },
+            {
+              icon: "/class.png",
+              label: "Classes",
+              href: "/list/classes",
+              visible: ["admin", "teacher"],
+            },
+            {
+              icon: "/lesson.png",
+              label: "Lessons",
+              href: "/list/lessons",
+              visible: ["admin", "teacher"],
+            },
+            {
+              icon: "/exam.png",
+              label: "Exams",
+              href: "/list/exams",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/assignment.png",
+              label: "Assignments",
+              href: "/list/assignments",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/result.png",
+              label: "Results",
+              href: "/list/results",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/attendance.png",
+              label: "Attendance",
+              href: "/list/attendance",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/calendar.png",
+              label: "Events",
+              href: "/list/events",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/message.png",
+              label: "Messages",
+              href: "/list/messages",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/announcement.png",
+              label: "Announcements",
+              href: "/list/announcements",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+          ],
+        },
+        {
+          title: "OTHER",
+          items: [
+            {
+              icon: "/profile.png",
+              label: "Profile",
+              href: "/profile",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/setting.png",
+              label: "Settings",
+              href: "/settings",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+              icon: "/logout.png",
+              label: "Logout",
+              href: "/logout",
+              visible: ["admin", "teacher", "student", "parent"],
+            },
+          ],
+        },
+      ];
+  return (
+    <div>
+        {
+            menuItems.map(({items,title},index)=>{
+               
+                return(
+                    <div key={index}>
+                        <span className='hidden md:block text-gray-400 font-light '>{title}</span>
+                       {items.map(({icon,label,href,visible},index)=>{
+                        return (
+                           <Link href={href} key={index} className='flex justify-center items-center lg:justify-start gap-2 mt-2 text-sm hover:bg-gray-200 hover:rounded-md'>  
+                           <Image src={icon} alt='img' width={20} height={20}></Image>
+                           <span className='hidden lg:block'>{label}</span>
+                            </Link>
+                        )
+                       })}
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default Menu
