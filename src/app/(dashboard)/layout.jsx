6e65b40d4391d8441f235c24fcab49b56d8f7518
MajidKh230 <khanmajid230@gmail.com>
2024-10-29 23:27:59 +0500

    worked on dashoboard

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,13 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Dashboardlayout = ({ children }) => {
   return (
     <div className="w-full h-screen flex ">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">l</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+<Link href={"/"} className="flex justify-center lg:justify-start items-center gap-2 ">
+<Image src="/logo.png" alt="logo" width={32} height={32}></Image>
+<span className="hidden md:block font-bold">APS School</span>
+</Link>
+      </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-        r
+      {children}
       </div>
     </div>
   );
